9be7d340e86d6a4bb15bb2982a505388be4f268e
Alejandra V <134009912+Malejav18@users.noreply.github.com>
2025-09-29 00:22:18 -0500

    Update Demo.java

--- Bridge/Demo.java
+++ Bridge/Demo.java
@@ -6,6 +6,9 @@
 /**
  *
  * @author maleja
+ * Método main para ejecutar y probar la implementación 
+ * del patrón Bridge usando diferentes dispositivos (Tv, Radio) y diferentes 
+ * controles remotos (BasicRemote, AdvancedRemote).
  */
 
 import Devices.Device;
@@ -16,20 +19,29 @@ import Remotes.BasicRemote;
 
 public class Demo {
     public static void main(String[] args) {
-        testDevice(new Tv());
-        testDevice(new Radio());
+        testDevice(new Tv()); // Pruebas con un televisor
+        testDevice(new Radio()); // Pruebas con un radio
     }
 
+    /**
+     * Recibe cualquier dispositivo que implemente la interfaz device
+     * Primero prueba el control remoto básico y luego el avanzado
+     * muestra el estado del dispositivo.
+     *
+     * @param device instancia de un dispositivo concreto (Tv o Radio)
+     */
     public static void testDevice(Device device) {
+        // Prueba del control remoto básico
         System.out.println("Tests with basic remote.");
-        BasicRemote basicRemote = new BasicRemote(device);
-        basicRemote.power();
-        device.printStatus();
+        BasicRemote basicRemote = new BasicRemote(device); // Se crea un control remoto básico para el dispositivo
+        basicRemote.power(); // Enciende o apaga el dispositivo
+        device.printStatus(); // Muestra en consola el estado actual del dispositivo
 
+        // Prueba del control remoto avanzado
         System.out.println("Tests with advanced remote.");
-        AdvancedRemote advancedRemote = new AdvancedRemote(device);
-        advancedRemote.power();
-        advancedRemote.mute();
-        device.printStatus();
+        AdvancedRemote advancedRemote = new AdvancedRemote(device); // Se crea un control remoto avanzado para el mismo dispositivo
+        advancedRemote.power(); // Enciende o apaga el dispositivo
+        advancedRemote.mute();  // Silencia el dispositivo
+        device.printStatus();   // Muestra en consola el estado actual del dispositivo
     }
 }
