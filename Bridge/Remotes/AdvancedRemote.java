168040f7c8346f1f4e16b801f040840e07409890
Alejandra V <134009912+Malejav18@users.noreply.github.com>
2025-09-29 01:54:44 -0500

    Update AdvancedRemote.java

--- Bridge/Remotes/AdvancedRemote.java
+++ Bridge/Remotes/AdvancedRemote.java
@@ -7,18 +7,21 @@ import Devices.Device;
 
 /**
  *
- * @author maleja
+ * @author Lopez, Vargas
+ * 
+ * Control remoto avanzado
+ * Hereda de BasicRemote y agrega funcionalidad extra
  */
 
 
 public class AdvancedRemote extends BasicRemote {
-
+    // Constructor que inicializa el control remoto avanzado con un dispositivo.
     public AdvancedRemote(Device device) {
-        super.device = device;
+        super.device = device; // Asigna el dispositivo a la clase base
     }
 
-    public void mute() {
-        System.out.println("Remote: mute");
-        device.setVolume(0);
+    public void mute() { // Silencia el dispositivo
+        System.out.println("Remote: mute"); // Primero imprime un mensaje indicando la acción
+        device.setVolume(0); // Modifica el volumen
     }
 }
